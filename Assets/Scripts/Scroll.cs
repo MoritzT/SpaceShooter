f8f1298aca7eae396a9e6ac95f6275deb9ad4723
MoritzT <moritz.tolxdorff+github@gmail.com>
2018-05-28 00:22:34 -0400

    update

---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour {
+
+    public float speed = 0.2f;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        Vector2 offset = new Vector2(0, Time.time * speed);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+	}
+}
